859a261b22aeb50b6d1283de15e482d071f66de6
joe-toni <joefaburrieta@gmail.com>
2021-11-25 00:54:41 -0800

    troubleshooting heroku, checking to see if getting more specific with the file names on references helps

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Blog, Comment } = require('../../models');
+const { User, Blog, Comment } = require('../../models/index');
 
 
 
--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Blog } = require('../../models');
+const { User, Blog } = require('../../models/index');
 
 
 
